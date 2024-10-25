4048d020db2ef68f75cbc9ba077f9fea0cbbddbb
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-25 15:51:05 +0530

    Update banall.py

--- RAUSHAN/plugins/basic/banall.py
+++ RAUSHAN/plugins/basic/banall.py
@@ -1,12 +1,21 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+# Define your pyrogram client instance
+app_pyrogram = Client("my_bot")
+
 @app_pyrogram.on_message(
     filters.command("banall") & filters.group
 )
 async def banall_command(client, message: Message):
-    print("Getting members from {}".format(message.chat.id))
-    async for i in app_pyrogram.get_chat_members(message.chat.id):
+    print(f"Getting members from {message.chat.id}")
+    async for member in client.get_chat_members(message.chat.id):
         try:
-            await app_pyrogram.ban_chat_member(chat_id=message.chat.id, user_id=i.user.id)
-            print("Kicked {} from {}".format(i.user.id, message.chat.id))
+            await client.ban_chat_member(chat_id=message.chat.id, user_id=member.user.id)
+            print(f"Kicked {member.user.id} from {message.chat.id}")
         except Exception as e:
-            print("Failed to kick {}: {}".format(i.user.id, e))
+            print(f"Failed to kick {member.user.id}: {e}")
     print("Process completed")
+
+# Ensure to run the client
+app_pyrogram.run()
