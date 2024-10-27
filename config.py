86d503fa674a3082514ed7620cddf197518af0c8
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-10-27 23:23:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,16 +9,16 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "27079591")) #optional
 API_HASH = getenv("API_HASH", "c81ae4c3dc026ea4bf49842a8ce4a5f9") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7403621976").split()))
-OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7531575025").split()))
+OWNER_ID = int(getenv("OWNER_ID", "7445883361"))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-BOT_TOKEN = getenv("BOT_TOKEN", "7702205771:AAHukLTHGsyBl3rNQbefwQys5-TJdppVL_E")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/w04ren.jpg')
+BOT_TOKEN = getenv("BOT_TOKEN", "7308974963:AAEb76LkMiMQ174cFhazX-OV5fRmPpTYKKo")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/tk0u6c.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/BABY-MUSIC/SATYA_USERBOT")
+REPO_URL = getenv("REPO_URL", "https://github.com/istkharalam62/ISTKHAR_USERBOT")
 BRANCH = getenv("BRANCH", "main") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
